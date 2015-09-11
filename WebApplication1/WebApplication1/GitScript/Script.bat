37cc4305ec40ec4482b98075f92b18ff4818f334
parvathymohan <parvathy.mohan@rapidvaluesolutions.com>
2015-09-11 13:55:16 +0530

    Test

--- WebApplication1/WebApplication1/GitScript/Script.bat
+++ WebApplication1/WebApplication1/GitScript/Script.bat
@@ -1,6 +1,4 @@
 git config user.email "parvathy.mohan@rapidvaluesolutions.com"
 git config user.name "parvathymohan"
 git merge origin/DevBranch
-git config push.default simple
-git push origin HEAD:master
 
