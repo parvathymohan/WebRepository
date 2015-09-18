da45c692648bcd79a62c00c0c60980d2e1c58704
parvathymohan <parvathy.mohan@rapidvaluesolutions.com>
2015-09-18 14:42:18 +0530

    c

--- WebApplication1/WebApplication1/GitScript/Script.bat
+++ WebApplication1/WebApplication1/GitScript/Script.bat
@@ -1,5 +1,9 @@
 git config user.email "parvathy.mohan@rapidvaluesolutions.com"
 git config user.name "parvathymohan"
 
-git push origin master
-
+git checkout DevBranch
+git pull 
+git checkout master
+git pull
+git merge --no-ff --no-commit DevBranch
+git  push origin DevBranch
