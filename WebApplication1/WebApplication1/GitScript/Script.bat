4cf0645417bdbe2d35463d7380e5cc2b4898e157
parvathymohan <parvathy.mohan@rapidvaluesolutions.com>
2015-09-25 12:41:16 +0530

    Update Script.bat

--- WebApplication1/WebApplication1/GitScript/Script.bat
+++ WebApplication1/WebApplication1/GitScript/Script.bat
@@ -1,9 +1,5 @@
 git config user.email "parvathy.mohan@rapidvaluesolutions.com"
 git config user.name "parvathymohan"
 
-git checkout DevBranch
-git pull 
-git checkout master
-git pull
-git merge --no-ff --no-commit DevBranch
-git  push origin DevBranch
+git merge origin/devBranch
+git push -u origin –all
